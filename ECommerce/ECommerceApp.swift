7cfe3cf152e584bc5ad9d96f51ae5c76b7bb73ee
bovshevajoulia <bovsheva.joulia@gmail.com>
2022-08-24 19:34:20 +0700

    Initial Commit

---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by bovsheva on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
